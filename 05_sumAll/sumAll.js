11ccb41a20af4ca774978b1dcbd0dcf8fd06ee07
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-05-07 20:00:04 +0300

    Finish 05, 06, 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  return typeof a === "number" && typeof b === "number" && a >= 0 && b >= 0 ? (a + b) * (Math.abs(b - a) + 1) / 2 : "ERROR";
 };
 
 // Do not edit below this line
