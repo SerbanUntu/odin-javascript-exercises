91243851a44a34c80caa8fd406760b45af6deecc
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-05-07 19:43:12 +0300

    Finish 03, 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...elements) {
+  result = [];
+  itemLoop:
+  for (item of arr) {
+    for (element of elements) {
+      if (element === item) continue itemLoop;
+    }
+    result.push(item);
+  }
+  return result;
 };
 
 // Do not edit below this line
