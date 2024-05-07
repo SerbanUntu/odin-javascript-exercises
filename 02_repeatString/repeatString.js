247119736a9bc27fbe29527979b545e84dbc2217
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-05-07 19:36:01 +0300

    Finish 01, 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+  try {
+    return str.repeat(times);
+  }
+  catch(e) {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
