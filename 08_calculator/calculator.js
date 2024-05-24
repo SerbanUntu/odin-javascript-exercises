b81512a8d67b58df991af5702c5b8f926d6af082
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-05-24 12:50:32 +0300

    Finish 08, 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((t, c) => t += c, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((t, c) => t *= c, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	return new Array(x).fill(0).map((_, i) => i + 1).reduce((t, c) => t *= c, 1);
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.split('').filter(x => x.match(/[a-zA-Z0-9]/)).map(x => x.toLowerCase());
+  return str.join('') === str.reverse().join('');
 };
 
 // Do not edit below this line
