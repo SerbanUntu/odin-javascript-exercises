e6bdd0b23d8c65ff7095ce43568f6be3b8b19b04
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-05-24 13:28:51 +0300

    Finish 10, 11, 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  return arr.reduce((t, c) => {
+    t.push(c.title);
+    return t;
+  }, []);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  return arr
+    .reduce((t, c) => {
+      let first = 0;
+      let second = 0;
+      if("yearOfDeath" in t) {
+        first = t.yearOfDeath - t.yearOfBirth;
+      } else {
+        first = 2024 - t.yearOfBirth;
+      }
+      if("yearOfDeath" in c) {
+        second = c.yearOfDeath - c.yearOfBirth;
+      } else {
+        second = 2024 - c.yearOfBirth;
+      }
+      console.log(first, second);
+      return first > second ? t : c;
+    });
 };
 
 // Do not edit below this line
